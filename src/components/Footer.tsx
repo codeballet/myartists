721c3b32ecb103b76442fc66c3c3613f18bff64d
stjernholm <stjernholm@msn.com>
2024-10-05 20:08:02 +0200

    feat: Add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import { ReactElement } from "react";
 export function Footer(): ReactElement {
     return (
         <footer>
-            <p>footer</p>
+            <p>&copy; Johan Stjernholm, 2024</p>
+            <p>About</p>
         </footer>
     );
 }
